fef6a4cadcd4a7dab675c5116fd5f039c07a4011
vanyashau <konoval-iva@yandex.ru>
2025-08-24 20:29:40 +0300

    переименование русской папки на английский, избавление от глобальной переменной side, исправлена ошибка imagine->image, переименование функций для более чёткого отражения её сути конструкции в  lvls упрощены, функция field_drawer() вынесена за пределы main_game_loop() и переименована

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ pygame.init()
 #   loop -> с англ. 'петля', в контексте программы
 #   обозначает основной код в виде функции, который как бы идет по петле
 
-all_lvls = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
 side = 64
 
 class Cell:
@@ -34,18 +32,18 @@ class Cell:
         return self.rotation == self.win_pos
 
     def draw(self, surface, x, y, side, all_active):
-        imagine = self.get_image(all_active)
-        imagine = pygame.transform.scale(imagine, (side, side))
-        surface.blit(imagine, (x * side, y * side))
+        image = self.get_image(all_active)
+        image = pygame.transform.scale(image, (side, side))
+        surface.blit(image, (x * side, y * side))
 
-    def rotate_imagine(imagine, rotation):
+    def rotate_image(image, rotation):
         if rotation == 2:
-            return pygame.transform.rotate(imagine, -90)
+            return pygame.transform.rotate(image, -90)
         elif rotation == 3:
-            return pygame.transform.rotate(imagine, 180)
+            return pygame.transform.rotate(image, 180)
         elif rotation == 4:
-            return pygame.transform.rotate(imagine, 90)
-        return imagine
+            return pygame.transform.rotate(image, 90)
+        return image
 
 class EmptyCell(Cell):
     def get_image(self, all_active=False):
@@ -54,46 +52,46 @@ class EmptyCell(Cell):
 class BatteryCell(Cell):
     def get_image(self, all_active=False):
         if all_active:
-            imagine = pygame.image.load('items/battery_active.png')
+            image = pygame.image.load('items/battery_active.png')
         else:
-            imagine = pygame.image.load('items/battery.png')
-        return Cell.rotate_imagine(imagine, self.rotation)
+            image = pygame.image.load('items/battery.png')
+        return Cell.rotate_image(image, self.rotation)
 
 class LightCell(Cell):
     def get_image(self, all_active=False):
         if all_active:
-            imagine = pygame.image.load('items/light_active.png')
+            image = pygame.image.load('items/light_active.png')
         else:
-            imagine = pygame.image.load('items/light.png')
-        return Cell.rotate_imagine(imagine, self.rotation)
+            image = pygame.image.load('items/light.png')
+        return Cell.rotate_image(image, self.rotation)
 
 class StraightCell(Cell):
     def get_image(self, all_active=False):
         if all_active:
-            imagine = pygame.image.load('items/straight_active.png')
+            image = pygame.image.load('items/straight_active.png')
         else:
-            imagine = pygame.image.load('items/straight.png')
+            image = pygame.image.load('items/straight.png')
         if self.rotation == 2:
-            imagine = pygame.transform.rotate(imagine, -90)
-        return imagine
+            image = pygame.transform.rotate(image, -90)
+        return image
 
 class CornerCell(Cell):
     def get_image(self, all_active=False):
         if all_active:
-            imagine = pygame.image.load('items/corner_active.png')
+            image = pygame.image.load('items/corner_active.png')
         else:
-            imagine = pygame.image.load('items/corner.png')
-        return Cell.rotate_imagine(imagine, self.rotation)
+            image = pygame.image.load('items/corner.png')
+        return Cell.rotate_image(image, self.rotation)
 
 class CrossCell(Cell):
     def get_image(self, all_active=False):
         if all_active:
-            imagine = pygame.image.load('items/cross_active.png')
+            image = pygame.image.load('items/cross_active.png')
         else:
-            imagine = pygame.image.load('items/cross.png')
-        return imagine
+            image = pygame.image.load('items/cross.png')
+        return image
 
-def cell_creator(cell_type, rotation, frozen, win):
+def create_cell(cell_type, rotation, frozen, win):
     if cell_type == 1:
         return EmptyCell(cell_type, rotation, frozen, win)
     elif cell_type == 2:
@@ -107,7 +105,7 @@ def cell_creator(cell_type, rotation, frozen, win):
     elif cell_type == 6:
         return CrossCell(cell_type, rotation, frozen, win)
 
-def matrix_reader(file_path):
+def read_matrix_from_txt(file_path):
     matrix = []
     with open(file_path, 'r') as file:
         for line in file:
@@ -117,11 +115,11 @@ def matrix_reader(file_path):
                 rotation = int(cell_str[1])
                 frozen = int(cell_str[2])
                 win = int(cell_str[3])
-                row.append(cell_creator(cell_type, rotation, frozen, win))
+                row.append(create_cell(cell_type, rotation, frozen, win))
             matrix.append(row)
     return matrix
 
-def win_inspector(matrix):
+def inspect_win(matrix):
     for row in matrix:
         for cell in row:
             if cell.cell_type != 1 and cell.cell_type !=6 and cell.frozen != 2:
@@ -129,17 +127,29 @@ def win_inspector(matrix):
                     return False
     return True
 
-def next_lvl(lvls):
+def choice_next_lvl(lvls):
     if not lvls:
-        lvls = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        lvls = list(range(1, 11))
     lvl = random.choice(lvls)
     lvls.remove(lvl)
     return lvl, lvls
 
-def main_game_loop(file_path):
-    global side
-    
-    matrix = matrix_reader(file_path)
+def draw_field(scene, matrix, side, all_active=False):
+    for y, row in enumerate(matrix):
+        for x, cell in enumerate(row):
+            if cell.cell_type == 1 or cell.frozen == 2:
+                image = pygame.image.load('items/empty.png')
+            else:
+                image = pygame.image.load('items/filled.png')
+            image = pygame.transform.scale(image, (side, side))
+            scene.blit(image, (side * x, side * y))
+            if cell.cell_type != 1:
+                cell.draw(scene, x, y, side, all_active)
+    pygame.display.update()
+
+
+def main_game_loop(file_path, side):
+    matrix = read_matrix_from_txt(file_path)
 
     W = len(matrix[0]) * side
     H = len(matrix) * side
@@ -147,20 +157,7 @@ def main_game_loop(file_path):
     pygame.display.set_caption(f'GameElectro - level {lvl}')
     pygame.display.set_icon(pygame.image.load('items/light_active.png'))
 
-    def field_drawer(all_active=False):
-        for y, row in enumerate(matrix):
-            for x, cell in enumerate(row):
-                if cell.cell_type == 1 or cell.frozen == 2:
-                    imagine = pygame.image.load('items/empty.png')
-                else:
-                    imagine = pygame.image.load('items/filled.png')
-                imagine = pygame.transform.scale(imagine, (side, side))
-                sc.blit(imagine, (side * x, side * y))
-                if cell.cell_type != 1:
-                    cell.draw(sc, x, y, side, all_active)
-        pygame.display.update()
-
-    field_drawer()
+    draw_field(sc, matrix, side)
     FPS = 10
     clock = pygame.time.Clock()
     game_won = False
@@ -180,13 +177,13 @@ def main_game_loop(file_path):
                 W = len(matrix[0]) * side
                 H = len(matrix) * side
                 sc = pygame.display.set_mode((W, H), pygame.RESIZABLE)
-                field_drawer(game_won)
+                draw_field(sc, matrix, side, game_won)
                 if game_won:
-                    restart_imagine = pygame.image.load('items/перезапуск.png')
-                    restart_imagine = pygame.transform.scale(restart_imagine, (side*2, side*2))
+                    restart_image = pygame.image.load('items/перезапуск.png')
+                    restart_image = pygame.transform.scale(restart_image, (side*2, side*2))
                     x_corner = (W - side*2) // 2
                     y_corner = (H - side*2) // 2
-                    sc.blit(restart_imagine, (x_corner, y_corner))
+                    sc.blit(restart_image, (x_corner, y_corner))
                     pygame.display.flip()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if not game_won and event.button == 1:
@@ -196,14 +193,14 @@ def main_game_loop(file_path):
                         cell = matrix[Y_quad][X_quad]
                         if cell.can_rotate():
                             cell.rotate()
-                            game_won = win_inspector(matrix)
-                            field_drawer(game_won)
+                            game_won = inspect_win(matrix)
+                            draw_field(sc, matrix, side, game_won)
                             if game_won:
-                                restart_imagine = pygame.image.load('items/перезапуск.png')
-                                restart_imagine = pygame.transform.scale(restart_imagine, (side*2, side*2))
+                                restart_image = pygame.image.load('items/перезапуск.png')
+                                restart_image = pygame.transform.scale(restart_image, (side*2, side*2))
                                 x_corner = (W - side*2) // 2
                                 y_corner = (H - side*2) // 2
-                                sc.blit(restart_imagine, (x_corner, y_corner))
+                                sc.blit(restart_image, (x_corner, y_corner))
                                 pygame.display.flip()
                 elif game_won and event.button == 1:
                     btn_size = side * 2
@@ -211,201 +208,15 @@ def main_game_loop(file_path):
                     y_corner = (H - btn_size) // 2
                     if (x_corner <= mouse_X <= x_corner + btn_size and
                         y_corner <= mouse_Y <= y_corner + btn_size):
-                        return True
+                        return True, side
         clock.tick(FPS)
-    return False
+    return False, side
 
-lvls = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+lvls = list(range(1, 11))
 
 while True:
-    lvl, lvls = next_lvl(lvls)
-    file_path = f'уровни/lvl-{lvl}.txt'
-    should_restart = main_game_loop(file_path)
+    lvl, lvls = choice_next_lvl(lvls)
+    file_path = f'levels/lvl-{lvl}.txt'
+    should_restart, side = main_game_loop(file_path, side)
     if not should_restart:
         break
-
-# def main_game_loop():
-#     SIDE = 64
-#     def load_scaled_image(path, size):
-#         img = pygame.image.load(path)
-#         return pygame.transform.scale(img, (size, size))
-
-#     def check_win(qurrent, win, type_of_quad, freez):
-#         for y in range(len(qurrent)):
-#             for x in range(len(qurrent[0])):
-#                 if type_of_quad[y][x] != 1 and freez[y][x] != 2:
-#                     if qurrent[y][x] != win[y][x]:
-#                         return False
-#         return True
-
-#     def reload_win(type_of_quad, win):
-#         match type_of_quad:
-#             case 1: return pygame.image.load('items/empty.png')
-#             case 2: img = pygame.image.load('items/battery_active.png')
-#             case 3: img = pygame.image.load('items/light_active.png')
-#             case 4: img = pygame.image.load('items/straight_active.png')
-#             case 5: img = pygame.image.load('items/corner_active.png')
-#             case 6: img = pygame.image.load('items/cross_active.png')
-
-#         rotation_state = win
-#         if rotation_state == 2: img = pygame.transform.rotate(img, -90)
-#         elif rotation_state == 3: img = pygame.transform.rotate(img, 180)
-#         elif rotation_state == 4: img = pygame.transform.rotate(img, 90)
-#         return img
-
-#     def reload(type_of_quad, qurrent):
-#         match type_of_quad:
-#             case 1: img = pygame.image.load('items/empty.png')
-#             case 2: img = pygame.image.load('./items/battery.png')
-#             case 3: img = pygame.image.load('items/light.png')
-#             case 4: img = pygame.image.load('items/straight.png')
-#             case 5: img = pygame.image.load('items/corner.png')
-#             case 6: img = pygame.image.load('items/cross.png')
-        
-#         rotation_state = qurrent
-#         if rotation_state == 2: img = pygame.transform.rotate(img, -90)
-#         elif rotation_state == 3: img = pygame.transform.rotate(img, 180)
-#         elif rotation_state == 4: img = pygame.transform.rotate(img, 90)
-#         return img
-
-#     def read_matrix_from_txt(file_path):
-#         with open(file_path, 'r') as file:
-#             return [[int(x) for x in line.strip().split()] for line in file]
-
-#     lvl = random.randint(1, 10)
-#     file_path = f'уровни/lvl-{lvl}.txt'
-#     matrix = read_matrix_from_txt(file_path)
-
-#     qurrent = copy.deepcopy(matrix)
-#     win = copy.deepcopy(matrix)
-#     type_of_quad = copy.deepcopy(matrix)
-#     freez = copy.deepcopy(matrix)
-
-#     for y in range(len(matrix)):
-#         for x in range(len(matrix[0])):
-#             type_of_quad[y][x] = int(str(matrix[y][x])[0])
-#             win[y][x] = int(str(matrix[y][x])[3])
-#             qurrent[y][x] = int(str(matrix[y][x])[1])
-#             freez[y][x] = int(str(matrix[y][x])[2])
-
-#     W = len(matrix[0]) * SIDE
-#     H = len(matrix) * SIDE
-#     sc = pygame.display.set_mode((W, H), pygame.RESIZABLE)
-#     pygame.display.set_caption(f'Электропроводка v. 1.0 - уровень {lvl}')
-#     pygame.display.set_icon(pygame.image.load('items/light_active.png'))
-
-#     for y in range(len(matrix)):
-#         for x in range(len(matrix[0])):
-#             if type_of_quad[y][x] == 1 or freez[y][x] == 2:
-#                 sc.blit(load_scaled_image('items/empty.png', SIDE), (SIDE * x, SIDE * y))
-#             else:
-#                 sc.blit(load_scaled_image('items/filled.png', SIDE), (SIDE * x, SIDE * y))
-    
-#     for y in range(len(matrix)):
-#         for x in range(len(matrix[0])):
-#             if type_of_quad[y][x] not in [1, 0]:
-#                 cell_img = reload(type_of_quad[y][x], qurrent[y][x])
-#                 sc.blit(pygame.transform.scale(cell_img, (SIDE, SIDE)), (SIDE * x, SIDE * y))
-
-#     pygame.display.update()
-
-#     FPS = 60
-#     clock = pygame.time.Clock()
-#     game_won = False
-#     running = True
-#     while running:
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 return False
-            
-#             elif event.type == pygame.MOUSEMOTION:
-#                 mouse_X, mouse_Y = event.pos
-            
-#             elif event.type == pygame.VIDEORESIZE:
-#                 new_width, new_height = event.size
-#                 cell_size_x = new_width // len(matrix[0])
-#                 cell_size_y = new_height // len(matrix)
-#                 SIDE = max(min(cell_size_x, cell_size_y), 50)
-                
-#                 W = len(matrix[0]) * SIDE
-#                 H = len(matrix) * SIDE
-#                 sc = pygame.display.set_mode((W, H), pygame.RESIZABLE)
-                
-#                 for y in range(len(matrix)):
-#                     for x in range(len(matrix[0])):
-#                         if type_of_quad[y][x] == 1 or freez[y][x] == 2:
-#                             img = load_scaled_image('items/empty.png', SIDE)
-#                         else:
-#                             img = load_scaled_image('items/filled.png', SIDE)
-#                         sc.blit(img, (SIDE * x, SIDE * y))
-                        
-#                         if type_of_quad[y][x] not in [1, 0]:
-#                             cell_img = reload(type_of_quad[y][x], qurrent[y][x])
-#                             scaled_cell = pygame.transform.scale(cell_img, (SIDE, SIDE))
-#                             sc.blit(scaled_cell, (SIDE * x, SIDE * y))
-                
-#                 if game_won:
-#                     restart_img = load_scaled_image('items/перезапуск.png', SIDE*2)
-#                     x_corner = (W - SIDE*2) // 2
-#                     y_corner = (H - SIDE*2) // 2
-#                     sc.blit(restart_img, (x_corner, y_corner))
-                
-#                 pygame.display.flip()
-            
-#             elif event.type == pygame.MOUSEBUTTONDOWN:
-#                 if not game_won and event.button == 1:
-#                     X_quad = int(mouse_X) // SIDE
-#                     Y_quad = int(mouse_Y) // SIDE
-#                     if freez[Y_quad][X_quad] != 2 and type_of_quad[Y_quad][X_quad] != 1:
-#                         if type_of_quad[Y_quad][X_quad] == 4 and qurrent[Y_quad][X_quad] >= 2:
-#                             qurrent[Y_quad][X_quad] = 1
-#                         elif type_of_quad[Y_quad][X_quad] == 6:
-#                             qurrent[Y_quad][X_quad] = 1
-#                         else:
-#                             qurrent[Y_quad][X_quad] = qurrent[Y_quad][X_quad] % 4 + 1
-                        
-#                         bg = load_scaled_image('items/filled.png', SIDE)
-#                         sc.blit(bg, (SIDE * X_quad, SIDE * Y_quad))
-                        
-#                         cell_img = reload(type_of_quad[Y_quad][X_quad], qurrent[Y_quad][X_quad])
-#                         scaled_cell = pygame.transform.scale(cell_img, (SIDE, SIDE))
-#                         sc.blit(scaled_cell, (SIDE * X_quad, SIDE * Y_quad))
-                        
-#                         pygame.display.update((SIDE * X_quad, SIDE * Y_quad, SIDE, SIDE))
-                        
-#                         if check_win(qurrent, win, type_of_quad, freez):
-#                             game_won = True
-#                             for y in range(len(matrix)):
-#                                 for x in range(len(matrix[0])):
-#                                     if type_of_quad[y][x] == 1 or freez[y][x] == 2:
-#                                         img = load_scaled_image('items/empty.png', SIDE)
-#                                     else:
-#                                         img = load_scaled_image('items/filled.png', SIDE)
-#                                     sc.blit(img, (SIDE * x, SIDE * y))
-                                    
-#                                     if type_of_quad[y][x] not in [1, 0]:
-#                                         cell_img = reload_win(type_of_quad[y][x], win[y][x])
-#                                         scaled_cell = pygame.transform.scale(cell_img, (SIDE, SIDE))
-#                                         sc.blit(scaled_cell, (SIDE * x, SIDE * y))
-                            
-#                             restart_img = load_scaled_image('items/перезапуск.png', SIDE*2)
-#                             x_corner = (W - SIDE*2) // 2
-#                             y_corner = (H - SIDE*2) // 2
-#                             sc.blit(restart_img, (x_corner, y_corner))
-#                             pygame.display.flip()
-                
-#                 elif game_won and event.button == 1:
-#                     btn_size = SIDE * 2
-#                     x_corner = (W - btn_size) // 2
-#                     y_corner = (H - btn_size) // 2
-                    
-#                     if (x_corner <= mouse_X <= x_corner + btn_size and 
-#                         y_corner <= mouse_Y <= y_corner + btn_size):
-#                         return True
-
-#         clock.tick(FPS)
-
-# while True:
-#     should_restart = main_game_loop()
-#     if not should_restart:
-#         break
